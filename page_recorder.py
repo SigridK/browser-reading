1ee25000e6c33e3c82e2eccbcb6f29230f508885
MariaBarrett <mria.barrett@gmail.com>
2015-10-12 15:03:07 +0200

    Committing though unfinished

--- page_recorder.py
+++ page_recorder.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask import request
+import codecs
+import argparser
+
+
+parser = argparse.ArgumentParser(
+    description="""Listens to EyeTribe ET output and saves it to a file""")
+parser.add_argument("outfilename",
+    help="Name of outfile e.g. outfilename.csv",
+    default='outfile.csv', nargs='?')
+args = parser.parse_args()
 
 app = Flask(__name__)
 
+fileout = codecs.open(args.outfilename, w, 'utf-8')
 
 @app.route('/', methods=['POST'])
 def hello_world():
 	print(str(request.form))
+	fileout.write(str(request.form))
 	return str(request.form)
 
 #	return 'Hello World!'
