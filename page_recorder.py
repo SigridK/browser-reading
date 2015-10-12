fe936d7a200745aad74413203cd4818d12fcc731
MariaBarrett <mria.barrett@gmail.com>
2015-10-12 15:47:10 +0200

    Think it's working - we should check it out together

--- page_recorder.py
+++ page_recorder.py
@@ -1,27 +1,32 @@
 from flask import Flask
 from flask import request
 import codecs
-import argparser
+import argparse
+import time
+import datetime
+
+timestampfname = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
 
 parser = argparse.ArgumentParser(
-    description="""Listens to EyeTribe ET output and saves it to a file""")
+    description="""Listens to javascript output and saves it to a file""")
 parser.add_argument("outfilename",
-    help="Name of outfile e.g. outfilename.csv",
-    default='outfile.csv', nargs='?')
+    help="Name of outfile e.g. outfilename.txt",
+    default='outfile.txt'+timestampfname, nargs='?')
 args = parser.parse_args()
 
 app = Flask(__name__)
 
-fileout = codecs.open(args.outfilename, w, 'utf-8')
+fileout = codecs.open('Pagerecordings/'+args.outfilename+timestampfname, 'w', 'utf-8')
 
 @app.route('/', methods=['POST'])
 def hello_world():
 	print(str(request.form))
 	fileout.write(str(request.form))
-	return str(request.form)
+	#return str(request.form)
 
 #	return 'Hello World!'
 
 
 app.run(debug=True)
+fileout.close()
